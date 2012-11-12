d09548e2725a5f450c31a7c5528d329c7f792d5e
dekokun <shintaro.kurachi@gmail.com>
2012-11-12 21:59:59 +0900

    vimperatorのセッティング追加

--- setting.sh
+++ setting.sh
@@ -1,4 +1,5 @@
 ln -s ~/rc/.zshenv ~/.zshenv
 ln -s ~/rc/.zshrc ~/.zshrc
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
+git clone git@github.com:dekokun/vimperator-setting.git ~/.vimperator
+ln -s ~/.vimperator/.vimperatorrc ~/.vimperatorrc
