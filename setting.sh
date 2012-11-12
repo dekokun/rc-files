1950d2c299ee4096e1e5782a2a8fcc1c45887c2c
dekokun <shintaro.kurachi@gmail.com>
2012-11-12 22:01:24 +0900

    コメント追加

--- setting.sh
+++ setting.sh
@@ -1,7 +1,12 @@
+# zshの設定
 ln -s ~/rc/.zshenv ~/.zshenv
 ln -s ~/rc/.zshrc ~/.zshrc
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# vimperatorの設定
 git clone git@github.com:dekokun/vimperator-setting.git ~/.vimperator
 ln -s ~/.vimperator/.vimperatorrc ~/.vimperatorrc
+
+# vimの設定
 git clone git@github.com:dekokun/vimruntime.git ~/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
