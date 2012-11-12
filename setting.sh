8b0aef49638a9bddf0dd9bd64009ca46fe422d90
dekokun <shintaro.kurachi@gmail.com>
2012-11-12 22:00:14 +0900

    vimのセッティング追加

--- setting.sh
+++ setting.sh
@@ -3,3 +3,5 @@ ln -s ~/rc/.zshrc ~/.zshrc
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone git@github.com:dekokun/vimperator-setting.git ~/.vimperator
 ln -s ~/.vimperator/.vimperatorrc ~/.vimperatorrc
+git clone git@github.com:dekokun/vimruntime.git ~/.vim
+ln -s ~/.vim/.vimrc ~/.vimrc
