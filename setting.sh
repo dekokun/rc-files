e23d91a2285f7cb6fb8d4b51926d1d1ee51770c5
dekokun <shintaro.kurachi@gmail.com>
2015-01-05 19:09:17 +0900

    いい感じに色がつくようにした

--- setting.sh
+++ setting.sh
@@ -9,6 +9,7 @@ ln -s ~/rc/.bash_profile ~/.bash_profile
 ln -s ~/rc/.zshenv ~/.zshenv
 ln -s ~/rc/.zshrc ~/.zshrc
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+(cd ~/.oh-my-zsh/custom/plugins && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git)
 
 # gitの設定
 ln -s ~/rc/.gitconfig ~/.gitconfig
